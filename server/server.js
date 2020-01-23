4c2db528e5a9a2bdd49298f0a53affb110b81268
Stryzhonok Danila <danstrig2000@mail.ru>
2020-01-24 01:30:32 +0300

    mongoDB connection
    I have added connection to local mongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoURI = require('./config/default').mongoURI;
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('mongoDB connected'))
+.catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('HELLO');
