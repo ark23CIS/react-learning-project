3f14390a9dddd4d316d551dc69783f1793f5cae0
Stryzhonok Danila <danstrig2000@mail.ru>
2020-01-23 23:59:58 +0300

    adding a server in project
    in this update i added an express server to my proj

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('HELLO');
+})
+
+app.listen(PORT, () => {
+    console.log(`server listening at ${PORT} port`);
+})
